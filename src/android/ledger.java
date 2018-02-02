9f6eba5248ab7d187c3f028cb399a1359b04ebd0
Gurpreet Singh <gurpreet@mailfence.com>
2018-02-02 02:35:33 -0500

    Narrow down basic interface to Ledger java API and setup JS-Android link using those methods

--- src/android/ledger.java
+++ src/android/ledger.java
@@ -8,25 +8,78 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * This class echoes a string called from JavaScript.
+ * This class handles the native Android operations of interacting with a Ledger device
+ * It provides an API for JS to call native methods using the execute method as a link
  */
 public class ledger extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
+        //TODO: When cleaning up use reflection to get the method
+        //Don't mess with it until functionality works
+        //method = this.getClass().getMethod(action, JSONArray.class, CallbackContext.class);
+        //method.invoke(args, callbackContext)
+
+        //Temporary for debugging
+        switch(action)
+        {
+            case "initTransport":
+                this.initTransport(args, callbackContext);
+                return true;
+            case "initDongle":
+                this.initDongle(args, callbackContext);
+                return true;
+            case "setup":
+                this.setup(args, callbackContext);
+                return true;
+            case "getPinRemainingAttempts":
+                this.getPinRemainingAttempts(args, callbackContext);
+                return true;
+            case "verifyPin":
+                this.verifyPin(args, callbackContext);
+                return true;
+            case "getWalletPublicKey":
+                this.getWalletPublicKey(args, callbackContext);
+                return true;
         }
+
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    //Sets up the TEE transport allowing connection to the device
+    //TODO: Link initialization steps because state needs to be maintained within Java (i think)
+    private void initTransport(JSONArray args, CallbackContext callbackContext)
+    {
+        //Success can take a JSONObject, JSONArray, or String
+        //Obviously can't send back a Transport object b/c it's going to JS
+        callbackContext.success("Transport initialized");
+        callbackContext.error("Cannot initialize transport");
     }
+
+    //Uses the TEE transport to create a BTDongle object
+    //TODO: Link initialization steps because state needs to be maintained within Java (i think)
+    private void initDongle(JSONArray args, CallbackContext callbackContext)
+    {
+    }
+
+    //Allows the creation of a new wallet
+    private void setup(JSONArray args, CallbackContext callbackContext)
+    {
+    }
+
+    //Returns the public key associated with the dongle
+    private void getWalletPublicKey(JSONArray args, CallbackContext callbackContext)
+    {
+    }
+
+    //Prompts for pin verification
+    private void verifyPin(JSONArray args, CallbackContext callbackContext)
+    {
+    }
+
+    //Returns how many pin attempts are remaining
+    private void getPinRemainingAttempts(JSONArray args, CallbackContext callbackContext)
+    {
+    }
+
 }
