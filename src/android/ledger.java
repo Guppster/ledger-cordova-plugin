adcec24f9761bad2b8101c473a9db97867538cb9
Gurpreet Singh <gurpreet@mailfence.com>
2018-04-23 22:20:03 -0500

    Exposed essential dongle functionality

--- src/android/ledger.java
+++ src/android/ledger.java
@@ -1,5 +1,7 @@
 package me.gurpreetsingh.cordova;
 
+import com.btchip.BTChipException;
+import com.btchip.comm.BTChipTransport;
 import com.btchip.comm.android.BTChipTransportAndroid;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -8,6 +10,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Context;
 import android.widget.Toast;
+import com.btchip.BTChipDongle;
 
 /**
  * This class handles the native Android operations of interacting with a Ledger device
@@ -15,8 +18,14 @@ import android.widget.Toast;
  */
 public class ledger extends CordovaPlugin
 {
+    private static final String DUMMY_PIN = "0000";
+
     private BTChipTransportAndroid device;
     private Context context;
+
+    private Toast toastSuccess = Toast.makeText(context, "success", Toast.LENGTH_LONG);
+    private Toast toastError = Toast.makeText(context, "error", Toast.LENGTH_LONG);
+    private BTChipDongle dongle;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -40,8 +49,6 @@ public class ledger extends CordovaPlugin
     private void init(CallbackContext callbackContext)
     {
         Toast toastFound = Toast.makeText(context, "device found", Toast.LENGTH_LONG);
-        Toast toastSuccess = Toast.makeText(context, "success", Toast.LENGTH_LONG);
-        Toast toastError = Toast.makeText(context, "error", Toast.LENGTH_LONG);
 
         //Create an interface to the device
         device = new BTChipTransportAndroid(context);
@@ -65,26 +72,71 @@ public class ledger extends CordovaPlugin
                 callbackContext.error("Connection failed or timed out");
             }
         });
+
+        BTChipTransport transport = device.getTransport();
+        dongle = new BTChipDongle(transport);
     }
 
     //Allows the creation of a new wallet
     private void setupWallet(JSONArray args, CallbackContext callbackContext)
     {
+        try
+        {
+            dongle.setup(new BTChipDongle.OperationMode[]{BTChipDongle.OperationMode.WALLET},
+                    new BTChipDongle.Feature[]{BTChipDongle.Feature.RFC6979},
+                    03,         //Standard address header goes here
+                    05,    //multisig address header goes here
+                    new byte[4],
+                    null,
+                    null,
+                    null,
+                    null
+                    );
+        }
+        catch (BTChipException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     //Returns the public key associated with the dongle
     private void getWalletPublicKey(JSONArray args, CallbackContext callbackContext)
     {
+        try
+        {
+            dongle.getWalletPublicKey("some key path");
+        }
+        catch (BTChipException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     //Prompts for pin verification
     private void verifyPin(JSONArray args, CallbackContext callbackContext)
     {
+        try
+        {
+            dongle.verifyPin(DUMMY_PIN.getBytes());
+        }
+        catch (BTChipException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
     //Returns how many pin attempts are remaining
     private void getPinRemainingAttempts(JSONArray args, CallbackContext callbackContext)
     {
+        try
+        {
+            dongle.getVerifyPinRemainingAttempts();
+        }
+        catch (BTChipException e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
--- www/ledger.js
+++ www/ledger.js
@@ -3,3 +3,19 @@ var exec = require('cordova/exec');
 exports.init = function init (success, error) {
     exec(success, error, 'ledger', 'init', []);
 };
+
+exports.setupWallet = function setupWallet (success, error) {
+    exec(success, error, 'ledger', 'setupWallet', []);
+};
+
+exports.getWalletPublicKey = function getWalletPublicAddress (success, error) {
+    exec(success, error, 'ledger', 'getWalletPublicAddress', []);
+};
+
+exports.verifyPin = function verifyPin (success, error) {
+    exec(success, error, 'ledger', 'verifyPin', []);
+};
+
+exports.getPinRemainingAttempts = function getPinRemainingAttempts (success, error) {
+    exec(success, error, 'ledger', 'getPinRemainingAttempts', []);
+};
